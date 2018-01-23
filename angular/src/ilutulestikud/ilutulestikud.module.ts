20487c335c1ba70e65f19b4a60289970a27adf5e
Ben O'Leary <benjamin.oleary@gmail.com>
2018-01-24 00:29:57 +0100

    Improve look of user interface.

--- angular/src/ilutulestikud/ilutulestikud.module.ts
+++ angular/src/ilutulestikud/ilutulestikud.module.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { IlutulestikudComponent } from './ilutulestikud.component';
 import { IlutulestikudService } from './ilutulestikud.service';
-
 
 @NgModule({
   declarations: [
@@ -17,14 +21,24 @@ import { IlutulestikudService } from './ilutulestikud.service';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatSelectModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   exports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
     MatSelectModule,
+    MatButtonModule,
     IlutulestikudComponent
   ],
   providers: [IlutulestikudService]
