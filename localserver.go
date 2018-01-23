b009819eb1393129fc2d290a4aa3063aa78f7456
Ben O'Leary <benjamin.oleary@gmail.com>
2018-01-23 22:53:02 +0100

    Remove serving static pages from server, as Google App Engine will be backend for static files hosted on Google Cloud Storage, while local development will use ng serve for serving static pages.

--- localserver.go
+++ localserver.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/benoleary/ilutulestikud/server"
 	"net/http"
-	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("This program needs exactly one argument, which is the directory to serve for the Angular code")
-		os.Exit(1)
-	}
-
-	angularDirectory := os.Args[1]
-	httpFileServer := http.FileServer(http.Dir(angularDirectory))
-	http.Handle("/client/", http.StripPrefix("/client", httpFileServer))
-
 	// We could load the allowed origin from a file, but this app is very specific to a set of fixed addresses.
 	serverState := server.CreateNew("http://localhost:4233")
 	http.HandleFunc("/backend/", serverState.HandleBackend)
