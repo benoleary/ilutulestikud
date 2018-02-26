13144e886ee427378086637525678be794236734
benoleary <benjamin.oleary@gmail.com>
2018-02-26 15:39:56 +0100

    Separate test into its own package.

--- server/server_test.go
+++ server/server_test.go
@@ -1,4 +1,4 @@
-package server
+package server_test
 
 // We do stick to the server package to make it easier to create server.State structs with mock handlers.
 
@@ -7,9 +7,33 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/benoleary/ilutulestikud/server"
 )
 
-// We do not bother testing the factory method New.
+// This just tests that the factory method does not cause any panics, and returns a non-nil pointer.
+func TestState_NewWithDefaultHandlers(testSet *testing.T) {
+	type testArguments struct {
+		accessControlAllowedOrigin string
+	}
+	testCases := []struct {
+		name      string
+		arguments testArguments
+	}{
+		{
+			name: "constructorDoesNotReturnNil",
+			arguments: testArguments{
+				accessControlAllowedOrigin: "irrelevant",
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		actualState := server.NewWithDefaultHandlers(testCase.arguments.accessControlAllowedOrigin)
+		if actualState == nil {
+			testSet.Errorf("%v. new state was nil.", testCase.name)
+		}
+	}
+}
 
 // We need a struct to mock the GET and POST handlers.
 type mockGetAndPostHandler struct {
@@ -31,146 +55,146 @@ func (mockHandler *mockGetAndPostHandler) HandlePost(httpBodyDecoder *json.Decod
 	return mockReturnStruct{Name: mockHandler.name, GivenSegments: relevantSegments[:]}, mockHandler.postCode
 }
 
-func prepareState(statusForGet int, statusForPost int) *State {
-	return &State{
-		accessControlAllowedOrigin: "irrelevant",
-		playerHandler:              &mockGetAndPostHandler{name: "player", getCode: statusForGet, postCode: statusForPost},
-		gameHandler:                &mockGetAndPostHandler{name: "game", getCode: statusForGet, postCode: statusForPost},
-	}
+func prepareState(statusForGet int, statusForPost int) *server.State {
+	return server.NewWithExplicitHandlers(
+		"irrelevant",
+		&mockGetAndPostHandler{name: "player", getCode: statusForGet, postCode: statusForPost},
+		&mockGetAndPostHandler{name: "game", getCode: statusForGet, postCode: statusForPost})
 }
 
-func TestState_HandleBackend(t *testing.T) {
-	type args struct {
+// This tests that the HandleBackend function selects the correct handler and the correct function of the handler.
+func TestState_HandleBackend(testSet *testing.T) {
+	type testArguments struct {
 		method  string
 		address string
 	}
 
-	type expected struct {
-		Struct *mockReturnStruct
-		Code   int
+	type expectedReturns struct {
+		returnStruct *mockReturnStruct
+		returnCode   int
 	}
 
-	tests := []struct {
-		name  string
-		state *State
-		args  args
-		expected
+	testCases := []struct {
+		name      string
+		state     *server.State
+		arguments testArguments
+		expected  expectedReturns
 	}{
 		{
-			name:  "getPlayer",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodGet, address: "/backend/player/test/get/player"},
-			expected: expected{
-				Struct: &mockReturnStruct{Name: "player", GivenSegments: []string{"test", "get", "player"}},
-				Code:   http.StatusOK,
+			name:      "getPlayer",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodGet, address: "/backend/player/test/get/player"},
+			expected: expectedReturns{
+				returnStruct: &mockReturnStruct{Name: "player", GivenSegments: []string{"test", "get", "player"}},
+				returnCode:   http.StatusOK,
 			},
 		},
 		{
-			name:  "postPlayer",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodPost, address: "/backend/player/test/post/player"},
-			expected: expected{
-				Struct: &mockReturnStruct{Name: "player", GivenSegments: []string{"test", "post", "player"}},
-				Code:   http.StatusOK,
+			name:      "postPlayer",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodPost, address: "/backend/player/test/post/player"},
+			expected: expectedReturns{
+				returnStruct: &mockReturnStruct{Name: "player", GivenSegments: []string{"test", "post", "player"}},
+				returnCode:   http.StatusOK,
 			},
 		},
 		{
-			name:  "getValidGame",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodGet, address: "/backend/game/test/get/game"},
-			expected: expected{
-				Struct: &mockReturnStruct{Name: "game", GivenSegments: []string{"test", "get", "game"}},
-				Code:   http.StatusOK,
+			name:      "getValidGame",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodGet, address: "/backend/game/test/get/game"},
+			expected: expectedReturns{
+				returnStruct: &mockReturnStruct{Name: "game", GivenSegments: []string{"test", "get", "game"}},
+				returnCode:   http.StatusOK,
 			},
 		},
 		{
-			name:  "postValidGame",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodPost, address: "/backend/game/test/post/game"},
-			expected: expected{
-				Struct: &mockReturnStruct{Name: "game", GivenSegments: []string{"test", "post", "game"}},
-				Code:   http.StatusOK,
+			name:      "postValidGame",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodPost, address: "/backend/game/test/post/game"},
+			expected: expectedReturns{
+				returnStruct: &mockReturnStruct{Name: "game", GivenSegments: []string{"test", "post", "game"}},
+				returnCode:   http.StatusOK,
 			},
 		},
 		{
-			name:  "getInvalidGame",
-			state: prepareState(http.StatusBadRequest, http.StatusOK),
-			args:  args{method: http.MethodGet, address: "/backend/game/test/get/game"},
-			expected: expected{
-				Struct: nil,
-				Code:   http.StatusBadRequest,
+			name:      "getInvalidGame",
+			state:     prepareState(http.StatusBadRequest, http.StatusOK),
+			arguments: testArguments{method: http.MethodGet, address: "/backend/game/test/get/game"},
+			expected: expectedReturns{
+				returnStruct: nil,
+				returnCode:   http.StatusBadRequest,
 			},
 		},
 		{
-			name:  "postInvalidGame",
-			state: prepareState(http.StatusOK, http.StatusBadRequest),
-			args:  args{method: http.MethodPost, address: "/backend/game/test/post/game"},
-			expected: expected{
-				Struct: nil,
-				Code:   http.StatusBadRequest,
+			name:      "postInvalidGame",
+			state:     prepareState(http.StatusOK, http.StatusBadRequest),
+			arguments: testArguments{method: http.MethodPost, address: "/backend/game/test/post/game"},
+			expected: expectedReturns{
+				returnStruct: nil,
+				returnCode:   http.StatusBadRequest,
 			},
 		},
 		{
-			name:  "getInvalidSegment",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodGet, address: "/backend/invalid/test/get/invalid"},
-			expected: expected{
-				Struct: nil,
-				Code:   http.StatusNotFound,
+			name:      "getInvalidSegment",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodGet, address: "/backend/invalid/test/get/invalid"},
+			expected: expectedReturns{
+				returnStruct: nil,
+				returnCode:   http.StatusNotFound,
 			},
 		},
 		{
-			name:  "postInvalidSegment",
-			state: prepareState(http.StatusOK, http.StatusOK),
-			args:  args{method: http.MethodPost, address: "/backend/invalid/test/post/invalid"},
-			expected: expected{
-				Struct: nil,
-				Code:   http.StatusNotFound,
+			name:      "postInvalidSegment",
+			state:     prepareState(http.StatusOK, http.StatusOK),
+			arguments: testArguments{method: http.MethodPost, address: "/backend/invalid/test/post/invalid"},
+			expected: expectedReturns{
+				returnStruct: nil,
+				returnCode:   http.StatusNotFound,
 			},
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			httpRequest := httptest.NewRequest(tt.args.method, tt.args.address, nil)
+	for _, testCase := range testCases {
+		testSet.Run(testCase.name, func(testSet *testing.T) {
+			httpRequest := httptest.NewRequest(testCase.arguments.method, testCase.arguments.address, nil)
 			// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 			responseRecorder := httptest.NewRecorder()
-			tt.state.HandleBackend(responseRecorder, httpRequest)
+			testCase.state.HandleBackend(responseRecorder, httpRequest)
 
-			if responseRecorder.Code != tt.expected.Code {
-				t.Errorf(
+			if responseRecorder.Code != testCase.expected.returnCode {
+				testSet.Errorf(
 					"%v returned wrong status %v instead of expected %v",
-					tt.name,
+					testCase.name,
 					responseRecorder.Code,
-					tt.expected.Code)
+					testCase.expected.returnCode)
 			}
 
-			if tt.expected.Struct != nil {
+			if testCase.expected.returnStruct != nil {
 				var actualStruct mockReturnStruct
 				decodingError := json.NewDecoder(responseRecorder.Body).Decode(&actualStruct)
 
 				if decodingError != nil {
-					t.Fatalf(
+					testSet.Fatalf(
 						"%v wrote undecodable JSON: error = %v",
-						tt.name,
+						testCase.name,
 						decodingError)
 				}
 
-				if actualStruct.Name != tt.expected.Struct.Name {
-					t.Fatalf(
+				if actualStruct.Name != testCase.expected.returnStruct.Name {
+					testSet.Fatalf(
 						"%v returned wrong struct %v instead of expected %v",
-						tt.name,
+						testCase.name,
 						actualStruct,
-						tt.expected.Struct)
+						testCase.expected.returnStruct)
 				}
 
-				for segmentIndex := 0; segmentIndex < len(tt.expected.Struct.GivenSegments); segmentIndex++ {
-					if actualStruct.GivenSegments[segmentIndex] != tt.expected.Struct.GivenSegments[segmentIndex] {
-						t.Fatalf(
+				for segmentIndex := 0; segmentIndex < len(testCase.expected.returnStruct.GivenSegments); segmentIndex++ {
+					if actualStruct.GivenSegments[segmentIndex] != testCase.expected.returnStruct.GivenSegments[segmentIndex] {
+						testSet.Fatalf(
 							"%v returned wrong struct %v instead of expected %v",
-							tt.name,
+							testCase.name,
 							actualStruct,
-							tt.expected.Struct)
+							testCase.expected.returnStruct)
 					}
 				}
 			}
