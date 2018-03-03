2d81864dd79309c153f87117de38505f37ef9251
Ben O'Leary <benjamin.oleary@gmail.com>
2018-03-03 09:29:39 +0100

    Show on command line that program has started and also remove some files which should never have been committed.

--- localserver.go
+++ localserver.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/benoleary/ilutulestikud/backend/server"
 )
 
 func main() {
+	fmt.Printf("Local server started.\n")
 	// We could load the allowed origin from a file, but this app is very specific to a set of fixed addresses.
 	serverState := server.NewWithDefaultHandlers("http://localhost:4233")
 	http.HandleFunc("/backend/", serverState.HandleBackend)
