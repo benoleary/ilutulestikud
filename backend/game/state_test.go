e846027d85beb16759f3b0741eadcf53fdce39ad
benoleary <benjamin.oleary@gmail.com>
2018-03-09 18:28:31 +0100

    Add test package to test game states specifically.

---
+++ backend/game/state_test.go
@@ -0,0 +1,110 @@
+package game_test
+
+import (
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/benoleary/ilutulestikud/backend/endpoint"
+	"github.com/benoleary/ilutulestikud/backend/game"
+	"github.com/benoleary/ilutulestikud/backend/player"
+)
+
+func prepareImplementations(
+	unitTest *testing.T,
+	gameName string,
+	playerNames []string) []game.State {
+	if len(playerNames) < game.MinimumNumberOfPlayers {
+		unitTest.Fatalf(
+			"Not enough players: %v",
+			playerNames)
+	}
+
+	nameToIdentifier := &endpoint.Base64NameEncoder{}
+	playerCollection := player.NewInMemoryCollection(nameToIdentifier, playerNames, []string{"red", "green", "blue"})
+	gameCollections := []game.Collection{
+		game.NewInMemoryCollection(nameToIdentifier),
+	}
+
+	playerIdentifiers := make([]string, len(playerNames))
+	for playerIndex, playerName := range playerNames {
+		playerIdentifiers[playerIndex] = nameToIdentifier.Identifier(playerName)
+	}
+
+	gameDefinition := endpoint.GameDefinition{
+		Name:    gameName,
+		Players: playerIdentifiers,
+	}
+
+	gameStates := make([]game.State, len(gameCollections))
+
+	for collectionIndex, gameCollection := range gameCollections {
+		addError := gameCollection.Add(gameDefinition, playerCollection)
+
+		if addError != nil {
+			unitTest.Fatalf(
+				"Error when trying to add game for collection index %v: %v",
+				collectionIndex,
+				addError)
+		}
+
+		// We find the game identifier by looking for all the games for the first
+		// player of the game definition, as there should only be one game in the
+		// collection, and it should include this player.
+		playerIdentifier := gameDefinition.Players[0]
+		allGames := gameCollection.All(playerIdentifier)
+
+		if (allGames == nil) || (len(allGames) != 1) {
+			unitTest.Fatalf(
+				"Error when trying to add find identifier for game for collection index %v: allGames = %v",
+				collectionIndex,
+				allGames)
+		}
+
+		gameStates[collectionIndex] = allGames[0]
+	}
+
+	return gameStates
+}
+
+func TestOrderByCreationTime(unitTest *testing.T) {
+	playerNames := []string{"Player One", "Player Two", "Player Three"}
+	earlyGameStates := prepareImplementations(
+		unitTest,
+		"Early game",
+		playerNames)
+
+	time.Sleep(100 * time.Millisecond)
+
+	lateGameStates := prepareImplementations(
+		unitTest,
+		"Late game",
+		playerNames)
+
+	for stateIndex := 0; stateIndex < len(lateGameStates); stateIndex++ {
+		gameList := game.ByCreationTime([]game.State{
+			lateGameStates[stateIndex],
+			earlyGameStates[stateIndex],
+		})
+
+		if !gameList[1].CreationTime().Before(gameList[0].CreationTime()) {
+			unitTest.Fatalf(
+				"Game states for state index %v were not differentiable by creation time: early at %v; late at %v",
+				stateIndex,
+				gameList[1].CreationTime(),
+				gameList[0].CreationTime())
+		}
+
+		sort.Sort(gameList)
+
+		if (gameList[0].Name() != earlyGameStates[stateIndex].Name()) ||
+			(gameList[1].Name() != lateGameStates[stateIndex].Name()) {
+			unitTest.Fatalf(
+				"Game states were not sorted: expected names [%v, %v], instead had [%v, %v]",
+				earlyGameStates[stateIndex].Name(),
+				lateGameStates[stateIndex].Name(),
+				gameList[0].Name(),
+				gameList[1].Name())
+		}
+	}
+}
