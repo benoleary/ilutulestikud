1ff23cf5fab77737f51a14b8c8deed429fef8701
benoleary <benjamin.oleary@gmail.com>
2018-05-06 19:16:38 +0200

    Move assertion into chat_test package as the chat log does not need to be tested again in another package.

--- backend/game/chat/log_test.go
+++ backend/game/chat/log_test.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/benoleary/ilutulestikud/backend/chat"
-	"github.com/benoleary/ilutulestikud/backend/chat/assertchat"
+	"github.com/benoleary/ilutulestikud/backend/game/chat"
 )
 
 func PrepareMessages(
@@ -83,11 +82,58 @@ func TestSortedLogAfterAppending(unitTest *testing.T) {
 
 			loggedMessages := chatLog.Sorted()
 
-			assertchat.LogIsCorrect(
+			assertLogIsCorrect(
 				unitTest,
 				testCase.name,
 				testCase.arguments.messagesToAppend,
 				loggedMessages)
 		})
+	}
+}
+
+// assertLogIsCorrect fails the given unit test if the given slices do not match,
+// with the logical equivalent of first padding out the expected messages list
+// by prepending empty messages until it is the correct length, and truncating
+// the actual messages from the front so that it is also the correct length.
+func assertLogIsCorrect(
+	unitTest *testing.T,
+	testIdentifier string,
+	expectedWithoutEmpties []chat.Message,
+	actualWithEmpties []chat.Message) {
+	if len(actualWithEmpties) != chat.LogSize {
+		unitTest.Fatalf(
+			testIdentifier+" - log did not have correct size: expected %v messages, but log was %v",
+			chat.LogSize,
+			actualWithEmpties)
+	}
+
+	numberOfSentMessages := len(expectedWithoutEmpties)
+
+	// We work our way backwards from the latest message.
+	for reverseIndex := 1; reverseIndex <= chat.LogSize; reverseIndex++ {
+		loggedMessage := actualWithEmpties[chat.LogSize-reverseIndex]
+
+		if reverseIndex > numberOfSentMessages {
+			if (loggedMessage.PlayerName != "") ||
+				(loggedMessage.ChatColor != "") ||
+				(loggedMessage.MessageText != "") {
+				unitTest.Errorf(
+					"Expected empty message with index %v, instead was %v",
+					chat.LogSize-reverseIndex,
+					loggedMessage)
+			}
+		} else {
+			expectedMessage :=
+				expectedWithoutEmpties[numberOfSentMessages-reverseIndex]
+			if (loggedMessage.PlayerName != expectedMessage.PlayerName) ||
+				(loggedMessage.ChatColor != expectedMessage.ChatColor) ||
+				(loggedMessage.MessageText != expectedMessage.MessageText) {
+				unitTest.Errorf(
+					"For log index %v, expected %v, instead was %v",
+					chat.LogSize-reverseIndex,
+					expectedMessage,
+					loggedMessage)
+			}
+		}
 	}
 }
