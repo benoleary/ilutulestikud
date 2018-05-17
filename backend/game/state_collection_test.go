8dd7bb986b812e2817d0faf105296fa617c77be0
benoleary <benjamin.oleary@gmail.com>
2018-05-17 13:13:06 +0200

    Salvage some relevant code and delete obsolete file.

--- backend/game/state_collection_test.go
+++ backend/game/state_collection_test.go
@@ -25,6 +25,69 @@ func TestViewsCorrectFromAllForPlayer(unitTest *testing.T) {
 }
 
 func TestRejectAddNewForEmptyGameName(unitTest *testing.T) {
+	validGameName := "Test game"
+
+	validPlayerNameList :=
+		[]string{
+			playerNamesAvailableInTest[0],
+			playerNamesAvailableInTest[1],
+			playerNamesAvailableInTest[2],
+		}
+
+	testCases := []struct {
+		testName    string
+		gameName    string
+		playerNames []string
+	}{
+		{
+			testName:    "Empty game name",
+			gameName:    "",
+			playerNames: validPlayerNameList,
+		},
+		{
+			testName:    "Nil players",
+			gameName:    validGameName,
+			playerNames: nil,
+		},
+		{
+			testName:    "No players",
+			gameName:    validGameName,
+			playerNames: []string{},
+		},
+		{
+			testName: "Too few players",
+			gameName: validGameName,
+			playerNames: []string{
+				playerNamesAvailableInTest[0],
+			},
+		},
+		{
+			testName:    "Too many players",
+			gameName:    validGameName,
+			playerNames: playerNamesAvailableInTest,
+		},
+		{
+			testName: "Repeated player",
+			gameName: validGameName,
+			playerNames: []string{
+				playerNamesAvailableInTest[2],
+				playerNamesAvailableInTest[1],
+				playerNamesAvailableInTest[1],
+				playerNamesAvailableInTest[3],
+			},
+		},
+		{
+			testName: "Unregistered player",
+			gameName: validGameName,
+			playerNames: []string{
+				playerNamesAvailableInTest[2],
+				playerNamesAvailableInTest[1],
+				"Not A. Registered Player",
+				playerNamesAvailableInTest[3],
+			},
+		},
+	}
+
 	unitTest.Fatalf("Not implemented yet")
 }
 
